24f251c2095cf31afd6465b5b6e1b3b883346248
Arm4nd7 <armando.estevezt@gmail.com>
2025-10-03 03:06:30 -0500

    mejora en body y nav

--- src/script.js
+++ src/script.js
@@ -1,11 +1,14 @@
 let buttonMenu = document.querySelector('.button-menu');
 let iconMenu = document.getElementById('icon-menu');
+let menu = document.querySelector('.menu');
 
 buttonMenu.addEventListener('click', function () {
-    if (iconMenu.className = "fa-solid fa-bars fa-xl") {
+    if (iconMenu.className == "fa-solid fa-bars fa-xl") {
         iconMenu.className = "fa-solid fa-arrow-right"
+        menu.style.display = "flex"
     } else {
         iconMenu.className = "fa-solid fa-bars fa-xl"
+        menu.style.display = "none"
     }
 })
 
