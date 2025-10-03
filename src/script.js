988150257586464f954fcd1331700b144189deee
Arm4nd7 <armando.estevezt@gmail.com>
2025-10-02 20:49:54 -0500

    refactorizando css

--- src/script.js
+++ src/script.js
@@ -1,35 +1,11 @@
-let menuBtn = document.querySelector('.button-menu');
-let closeMenu = document.querySelector('.close-nav');
-let fondoBlanco = document.querySelector(".logo-bienvenida-nav");
-let menu = document.querySelector("ul");
-let desplegable = document.querySelector(".lista-menu-desplegable");
-let noDesplegable = document.querySelector(".noDesplegable");
-let list = document.querySelectorAll("li");
+let buttonMenu = document.querySelector('.button-menu');
+let iconMenu = document.getElementById('icon-menu');
 
-function activeLink() {
-    list.forEach((item) =>
-        item.classList.remove('active'));
-    this.classList.add("active");
-}
-list.forEach((item) =>
-    item.addEventListener('click', activeLink));
-
-menuBtn.addEventListener("click", () => {
-    if (menu.classList.toggle("list-navigator")) {
-        closeMenu.className = "button-menu";
-        menu.classList.add("list-desplegable");
+buttonMenu.addEventListener('click', function () {
+    if (iconMenu.className = "fa-solid fa-bars fa-xl") {
+        iconMenu.className = "fa-solid fa-arrow-right"
+    } else {
+        iconMenu.className = "fa-solid fa-bars fa-xl"
     }
-    desplegable.style.display = "grid";
-    menuBtn.style.display = "none";
-
-});
-
-// cerrar menú
-closeMenu.addEventListener("click", () => {
-    desplegable.style.display = "none";
-    menuBtn.style.display = "block";
-    menu.classList.toggle("list-navigator")
-    menu.classList.remove("list-desplegable");
-    // fondoBlanco.style.display = "flex";
-});
+})
 
